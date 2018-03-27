877baf732662dbf85bbdc05fbb4019e0ad32a2fd
Kekchup <dementevio@mfcto.ru>
2018-03-27 16:51:28 +0500

    Fix

--- src/main/kotlin/MonoSamples.kt
+++ src/main/kotlin/MonoSamples.kt
@@ -1,24 +1,25 @@
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.MonoProcessor
+import java.time.Duration
 
 class MonoSamples {
     val LOG = LoggerFactory.getLogger(MonoSamples::class.java!!)
 
 
     @Throws(Exception::class)
-    fun main(args: Array<String>) {
+    fun main(args: Char) {
         // Deferred is the publisher, Promise the consumer
         val promise = MonoProcessor.create<String>()
 
         val result = promise.doOnSuccess({ p -> LOG.info("Promise completed {}", p) })
-                .doOnTerminate({s -> LOG.info("Got value: {}", s)})
-        //.doOnError { t -> LOG.error(t.message, t) }
-        //.subscribe()
+                .doOnTerminate{LOG.info("Got value:")}
+                .doOnError { t -> LOG.error(t.message, t) }
+
 
         promise.onNext("Hello World!")
-        //promise.onError(new IllegalArgumentException("Hello Shmello! :P"));
+        promise.onError( IllegalArgumentException("Hello Shmello! :P"));
 
-        val s = result.blockMillis(1000)
+        val s = result.block(Duration.ofMillis(1000))
         LOG.info("s={}", s)
     }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 
-
 fun main(args: Array<String>) {
-
+    val monoSamples= MonoSamples()
+    monoSamples.main("asd", "dasd")
+    val fluxSamples=FluxSamples()
+    fluxSamples.main('sad','asd')
 }
